cd2a1aa79bbfde42280bdd2e0b6c2d813f9a91c8
薛仁伟 <xrw2425467810@163.com>
2018-06-14 16:32:57 +0800

    vuex

--- project/src/pages/login/index.vue
+++ project/src/pages/login/index.vue
@@ -2,13 +2,13 @@
   <div class="loginContainer">
     <div id="particles-js"></div>
     <div id="loginWrap">
-      <el-tabs tab-position="center">
+      <el-tabs v-model="activeName" @tab-click="handleClick" tab-position="center">
         <el-tab-pane label="账号登录" name="first">
           <div id="loginForm">
             <el-form ref="loginForm" :model="loginForm" >
                 <el-input v-model="loginForm.username" placeholder="请输入姓名"></el-input>
                 <el-input v-model="loginForm.password" placeholder="请输入密码"></el-input>
-              <el-form-item>
+              <el-form-item style="text-align:center;margin-top:30px;">
                 <el-button type="primary" @click="submit">登录</el-button>
               </el-form-item>
             </el-form>
@@ -27,7 +27,9 @@
 <style>
 /* .loginContainer{position: relative;width: 100%;height: 100%;} */
 /* .loginContainer #particles-js{position: absolute;top: 0;left: 0;width: 100%;height: 100%;} */
-.loginContainer #loginWrap{position: absolute;top: 100px;left: 50%;width: 500px;margin-left:-250px;}
+.loginContainer #loginWrap{position: absolute;top: 100px;left: 50%;width: 400px;margin-left:-200px;}
+.el-tabs__nav{width: 100%;}
+.el-tabs__item {width: 50%;text-align: center;}
 </style>
 
 
@@ -43,21 +45,12 @@
         loginForm: {
           username: "",
           password: ""
-        }
+        },
+        activeName: "first"
       }
     },
     methods: {
       submit() {
-        console.log(this.loginForm.username)
-        console.log(this.loginForm.password)
-//        var params = new URLSearchParams()
-//        params.append("username",this.loginForm.username)
-//        params.append("password",this.loginForm.password)
-//        let postData = this.$qs.stringify({
-//          username:1,
-//          key2:2,
-//          key3:3,
-//        });
         axios.post('http://localhost:8001/login', {"username":1, "password": 2})
           .then(function(res) {
             console.log("------登录信息成功返回------" + res)
@@ -65,6 +58,9 @@
           .then(function(error) {
             console.log("------登录信息错误-------" + error);
           })
+      },
+      handleClick() {
+
       }
     }
   }
---
+++ project/src/store/store.js
@@ -0,0 +1,11 @@
+Vue.use(Vuex);
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        }
+    }
+})
